6b1aafd868da7a9f43b38737c498d7e07441e369
petercho0819 <dongr52@naver.com>
2021-01-23 13:31:56 +0900

    210123 13시 30분 두 번째 버전

--- src/team4/Manager.java
+++ src/team4/Manager.java
@@ -44,17 +44,18 @@ public class Manager {
 		String id  = sc.next();
 		sc.nextLine();
 		
-		
+		here :
+//		Passcheck();								// 비밀번호 재확인.
 		System.out.print("비밀번호를 입력하세요 : ");		// 비밀 번호 입력
 		String pwd = sc.nextLine();
 		System.out.print("비밀번호를 한 번 더 입력하세요 : ");	// 비밀 번호 재확인
-		String pwdDoubleCheck = sc.nextLine();			// 다른 클래스 생성
-//		if(!pwd.equals(pwdDoubleCheck)){
-//			System.out.println("비밀번호가 일치하지 않습니다");
-//			return ;
-//		}
-		
-		
+		String pwdDoubleCheck = sc.nextLine();
+		if(!pwd.equals(pwdDoubleCheck)){
+			System.out.println("비밀번호가 일치하지 않습니다");
+			System.out.println("비밀번호를 다시 입력하세요.");
+//			break here;
+			return;
+		}
 		System.out.print("이름을 입력하세요 : ");			// 이름 입력
 		String name = sc.nextLine();
 		System.out.print("태어난 년도를 입력하세요 : ");	// 태어난 년도 입력
@@ -66,9 +67,7 @@ public class Manager {
 		System.out.print("이메일을 입력하세요 : ");		// 이메일 입력
 		String email = sc.next();
 		
-//		if(id == Arraylist.) {
-//			System.out.println("중복된 아이디입니다.");
-//		}else 
+
 		
 			if(pwd.equals(pwdDoubleCheck)) {		// 비밀번호와 비밀번호 확인 입력
 				loginDTO = new LoginDTO(id, pwd, pwdDoubleCheck, name, year, month, day, email);	// 비밀번호 일치시 회원가입 정보를 배열로 저장
@@ -84,14 +83,15 @@ public class Manager {
 			}
 	}
 	
-//	public boolean idcheck(String id) {
-//		if()
+
+
+//	public void Passcheck() {
+		
+		
+//		}
 //		
-//		
-//		return false;
+//		return;
 //	}
-	
-	
 
 	public void login() {		//로그인 메소드
 		System.out.println("아이디를 입력하세요 : ");	// 아이디 입력
